38d10da84d54e33402b49e6140604031b488d432
Ge Wang <code.gewang@gmail.com>
2015-08-02 14:38:47 -0600

    Shrink window

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -26,7 +26,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
+	window = glfwCreateWindow( 640, 480, "Playground", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		glfwTerminate();
