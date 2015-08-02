267523625262b02466e7614e8669bf0e22d8d364
Ge Wang <code.gewang@gmail.com>
2015-08-02 14:36:35 -0600

    Make Mac OS X happy

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -22,6 +22,7 @@ int main( void )
 	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
